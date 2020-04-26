5ae070ef955b6cd2410a572a5e642810521e7726
Faisal Nawaz <31298666+faisalnawaz16@users.noreply.github.com>
2020-04-26 20:28:31 +0500

    Fixed Sign Up Process and Conncetivity to FIrebase

--- app/src/main/java/com/example/dietapp/Login_Signup.java
+++ app/src/main/java/com/example/dietapp/Login_Signup.java
@@ -17,72 +17,62 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Login_Signup extends AppCompatActivity {
-EditText email,password;
-Button btnSignIN;
-TextView txtSignIn;
-FirebaseAuth authFirebase;
+    EditText email, password;
+    Button btnSignIN;
+    TextView txtSignIn;
+    FirebaseAuth authFirebase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_signup);
 
-        authFirebase=FirebaseAuth.getInstance();
-        email=findViewById(R.id.editTextEmail);
-        password=findViewById(R.id.editTextPass);
-        txtSignIn=findViewById(R.id.textViewSignIn);
-        btnSignIN=findViewById(R.id.btnSignup);
+        authFirebase = FirebaseAuth.getInstance();
+        email = findViewById(R.id.editTextEmail);
+        password = findViewById(R.id.editTextPass);
+        txtSignIn = findViewById(R.id.textViewSignIn);
+        btnSignIN = findViewById(R.id.btnSignup);
 
         btnSignIN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String emailID= email.getText().toString();
-                String pwd= password.getText().toString();
-                if(emailID.isEmpty())
-                {
+                String emailID = email.getText().toString().trim();
+                String pwd = password.getText().toString().trim();
+                if (emailID.isEmpty()) {
                     email.setError("Please Enter Email");
                     email.requestFocus();
-                }
-                else if(pwd.isEmpty())
-                {
+                } else if (pwd.isEmpty()) {
                     password.setError("Enter Password");
                     password.requestFocus();
-                }
-                else if(emailID.isEmpty() && pwd.isEmpty())
-                {
+                } else if (emailID.isEmpty() && pwd.isEmpty()) {
                     email.setError("Please Enter Email");
                     password.setError("Enter Password");
                     email.requestFocus();
                     password.requestFocus();
-                    Toast.makeText(Login_Signup.this,"Fields are Empty",Toast.LENGTH_LONG).show();
+                    Toast.makeText(Login_Signup.this, "Fields are Empty", Toast.LENGTH_LONG).show();
                 }
-                else if(!emailID.isEmpty() && pwd.isEmpty())
-                {
+                else if (!(emailID.isEmpty() && pwd.isEmpty())) {
                     authFirebase.createUserWithEmailAndPassword(emailID,pwd).addOnCompleteListener(Login_Signup.this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
-                            if(!task.isSuccessful())
-                            {
-                                Toast.makeText(Login_Signup.this,"UnSuccessgull SignUp",Toast.LENGTH_LONG).show();
+                            if(!task.isSuccessful()){
+                                Toast.makeText(Login_Signup.this,"SignUp Unsuccessful, Please Try Again",Toast.LENGTH_SHORT).show();
                             }
-                            else
-                            {
-                                startActivity(new Intent(Login_Signup.this,set_diet_pcf.class));
-
+                            else {
+                                startActivity(new Intent(Login_Signup.this,login.class));
+                                Toast.makeText(Login_Signup.this,"SignUp Successful",Toast.LENGTH_SHORT).show();
                             }
                         }
                     });
-
                 }
-                else
-                {
-                    Toast.makeText(Login_Signup.this,"Error Occured",Toast.LENGTH_LONG).show();
+                else{
+                    Toast.makeText(Login_Signup.this,"There is some error please Try Again",Toast.LENGTH_LONG).show();
                 }
             }
         });
         txtSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 startActivity(new Intent(Login_Signup.this,login.class));
             }
         });
